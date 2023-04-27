98478736249cb1d8d4b020b8134d5b489db931e8
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-04-27 22:01:10 +0900

    00_03_確認課題

--- main.cpp
+++ main.cpp
@@ -1,12 +1,60 @@
 ﻿#include <Novice.h>
+#include <Struct.h>
+#include <Function.h>
 
-const char kWindowTitle[] = "LC1C_26_ツミタヒナタ_タイトル";
+
+const char kWindowTitle[] = "LE2B_20_ツミタヒナタ_タイトル";
+
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, Matrix4x4 matrix) {
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth, y + row * kRowHeight, "%.02f", matrix.m[row][column]);
+		}
+	}
+}
+
+void VectorScreenPrintf(int x, int y, Vector3 vector)
+{
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth + kColumnWidth, y, "%.02f", vector.z);
+}
+
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
+
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+	
+	Vector3 translate{ 4.1f, 2.6f, 0.8f };
+	
+	Vector3 scale{ 1.5f,5.2f,7.3f };
+
+	Vector3 point{ 2.3f,3.8f,1.4f };
+
+	Matrix4x4 trnsformMatrix =
+	{
+	1.0f,2.0f,3.0f,4.0f,
+	3.0f,1.0f,1.0f,2.0f,
+	1.0f,4.0f,2.0f,3.0f,
+	2.0f,2.0f,1.0f,3.0f
+	};
+
+	Vector3 trnsformed = Transform(point, trnsformMatrix);
+	
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+	
+	Matrix4x4 scalematrix = MakeeScaleMatrix(scale);
+
+
+
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -32,6 +80,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		VectorScreenPrintf(0, 0, trnsformed);
+		
+		MatrixScreenPrintf(0, 30, translateMatrix);
+		
+		MatrixScreenPrintf(0, 120, scalematrix);
 
 		///
 		/// ↑描画処理ここまで
